4daaaf9056b44b7055dc45c4061a336f8cd277d2
athanasiouconst <csiouath@gmail.com>
2019-07-29 19:15:09 +0300

    init commit react app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+//import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h1>React App : person-react-app</h1>
+                <Person/>
+            </div>
+        );
+    }
 }
+
+// class App extends Component {
+//     render() {
+//         return React.createElement(
+//             'div',
+//             {className: 'App'},
+//             React.createElement('h1', null, 'Hi, I\'m a React App called person-react-app'));
+//     }
+// }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+const person = () => {
+    return <p>My name is Constantinos Athanasiou!</p>
+};
+
+export default person;
